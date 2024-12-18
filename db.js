09b6c451e106b6c9e6d4ef5e08e713979a243dfe
Raviteja Mannam <ravitejamannam@Ravitejas-MacBook-Air.local>
2024-12-19 01:58:40 +0530

    added schema

---
+++ db.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+console.log('connected to Mongoose');
+mongoose.connect('mongodb+srv://ravitejamannam88:qaeuMivhDDN1E1z6@courseconnect.illiy.mongodb.net/')
+
+
+
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String,
+});
+
+const adminSchema = new Schema({
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String,
+});
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+});
+
+const purchaseSchema = new Schema({
+    userId: ObjectId,
+    courseId: ObjectId
+});
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.export = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
---
+++ routes/admin.js
@@ -0,0 +1,37 @@
+const { Router } = require("express");
+const adminRouter = Router();
+const { adminModel} =require("../db");
+
+adminRouter.post("/signup", function(req, res) {
+    res.json({
+        message: "admin signup endpoint"
+    })
+})
+
+adminRouter.post("/signin", function(req, res) {
+    res.json({
+        message: "admin signin endpoint"
+    })
+})
+
+adminRouter.post("/course", function(req, res) {
+    res.json({
+        message: "course endpoint"
+    })
+})
+
+adminRouter.put("/course", function(req, res) {
+    res.json({
+        message: "course endpoint"
+    })
+})
+
+adminRouter.get("/course/bulk", function(req, res) {
+    res.json({
+        message: "all courses endpoint"
+    })
+})
+
+module.exports = {
+    adminRouter: adminRouter
+}
