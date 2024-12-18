af2cff09b2c047771ee7ae27cfce6eb46d212fd0
Raviteja Mannam <ravitejamannam@Ravitejas-MacBook-Air.local>
2024-12-19 04:28:23 +0530

    added auth endpoints

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,48 @@
 const { Router } = require("express");
+const { userModel } = require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_USER_PASSWORD = "abcdefghijklmnopqrstuvwxyz";
 
 const userRouter = Router();
 
-userRouter.post("/signup", function(req, res) {
+userRouter.post("/signup", async function(req, res) {
+    const {  email, password , firstName , lastName } = req.body; 
+    await userModel.create({
+        email: email,
+        password :password,
+        firstName: firstName,
+        lastName: lastName,
+    })
+    
+    
     res.json({
-        message: "signup endpoint"
+        message: "signup successful",
     })
 })
 
-userRouter.post("/signin", function(req, res) {
-    res.json({
-        message: "signup endpoint"
+userRouter.post("/signin", async function(req, res) {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({
+        email: email,
+        password: password,
     })
+    if(user){
+        const token =jwt.sign({
+            id: user._id
+        }, JWT_USER_PASSWORD);
+
+        //TODO cookie logic/session logic
+        res.json({
+            token: token
+        })
+
+    } else {
+        res.status(403).json({
+            message: "Invalid credentials"
+        })
+    }
+    
 })
 
 userRouter.get("/purchases", function(req, res) {
