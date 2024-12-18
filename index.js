fe07a1a1716f0a5557011fb16323e58ea755863d
Raviteja Mannam <ravitejamannam88@gmail.com>
2024-12-19 04:28:23 +0530

    added auth endpoints

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
 const app = express();
 
+//middleware
+app.use(express.json()); 
+
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", courseRouter);
 app.use("/api/v1/admin", adminRouter);
--- routes/admin.js
+++ routes/admin.js
@@ -1,34 +1,69 @@
 const { Router } = require("express");
 const adminRouter = Router();
-const { adminModel } =require("../db");
+const { adminModel } = require("../db");
+const jwt = require("jsonwebtoken");
 
-adminRouter.post("/signup", function(req, res) {
+
+const JWT_ADMIN_PASSWORD = "123ldl1l23l3l";
+
+adminRouter.post("/signup", async function(req, res) {
+    const { email, password, firstName, lastName } = req.body; // TODO: adding zod validation
+    
+    await adminModel.create({
+        email: email,
+        password: password,
+        firstName: firstName, 
+        lastName: lastName
+    })
+    
     res.json({
-        message: "admin signup endpoint"
+        message: "Signup succeeded"
     })
 })
 
-adminRouter.post("/signin", function(req, res) {
-    res.json({
-        message: "admin signin endpoint"
-    })
+adminRouter.post("/signin", async function(req, res) {
+    const { email, password } = req.body;
+
+    
+    const admin = await adminModel.findOne({
+        email: email,
+        password: password
+    });
+
+    if (admin) {
+        const token = jwt.sign({
+            id: admin._id
+        }, JWT_ADMIN_PASSWORD);
+
+        
+
+        res.json({
+            token: token
+        })
+    } else {
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
 })
+
+
 
 adminRouter.post("/course", function(req, res) {
     res.json({
-        message: "course endpoint"
+        message: "signup endpoint"
     })
 })
 
 adminRouter.put("/course", function(req, res) {
     res.json({
-        message: "course endpoint"
+        message: "signup endpoint"
     })
 })
 
 adminRouter.get("/course/bulk", function(req, res) {
     res.json({
-        message: "all courses endpoint"
+        message: "signup endpoint"
     })
 })
 
