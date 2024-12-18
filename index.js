cdb31354a920a43dcb7254c038ac14a86b1833b6
Raviteja Mannam <ravitejamannam@Ravitejas-MacBook-Air.local>
2024-12-18 20:25:48 +0530

    added user and admin routes

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
 const express = require("express");
+const { userRouter } = require("./routes/user");
+const { courseRouter } = require("./routes/course");
 const app = express();
 
-
-app.post('/user/signup', function(req,res){
-    res.json({
-        message: "signup endpoint"
-    })
-})
-
-app.post('/user/signin', function(req,res){
-    res.json({
-        message: "signin endpoint"
-    })
-})
- app.get('/user/purchases', function(req,res){
-    res.json({
-        message: "purchases endpoint"
-    })
- })
-
- app.post('/course/purchase', function(req,res){
-    res.json({
-        message: "purchase endpoint"
-    })
- })
- app.get('/courses', function(req,res){
-res.json({
-    message: "courses endpoint"
-})
-    })
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/course", courseRouter);
 
 app.listen(3000);
