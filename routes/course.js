e1690b62daae7fb7b4cb74b058d7a71b81bec226
Raviteja Mannam <ravitejamannam@Ravitejas-MacBook-Air.local>
2024-12-19 22:12:20 +0530

    added last set of ebnd points

--- routes/course.js
+++ routes/course.js
@@ -1,24 +1,27 @@
 const { Router } = require("express");
 const { userMiddleware } = require("../middleware/user");
-const {purchaseModel } = require("../db");
+const { purchaseModel, courseModel } = require("../db")
 const courseRouter = Router();
 
-courseRouter.post("/purchase",userMiddleware,async function(req, res) {
-    const userId = req.userId ;
+courseRouter.post("/purchase", userMiddleware, async function(req, res) {
+    const userId = req.userId;
     const courseId = req.body.courseId;
 
+    // should check that the user has actually paid the price
     await purchaseModel.create({
-        userId: userId,
-        courseId: courseId
+        userId,
+        courseId
     })
+
     res.json({
-        message: "you have successfully purchased the course"
+        message: "You have successfully bought the course"
     })
 })
 
-courseRouter.get("/preview",async function(req, res) {
-
+courseRouter.get("/preview", async function(req, res) {
+    
     const courses = await courseModel.find({});
+
     res.json({
         courses
     })
