d0a8f16ba9cff1944781deb73b64bcc7d08e5a95
Raviteja Mannam <ravitejamannam88@gmail.com>
2024-12-19 05:51:40 +0530

    added admin endpoints

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,7 @@
 const { Router } = require("express");
 const { userModel } = require("../db");
 const jwt = require("jsonwebtoken");
-const JWT_USER_PASSWORD = "abcdefghijklmnopqrstuvwxyz";
-
+const { JWT_USER_PASSWORD } = require("../config");
 const userRouter = Router();
 
 userRouter.post("/signup", async function(req, res) {
