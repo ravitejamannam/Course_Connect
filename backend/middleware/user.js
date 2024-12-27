3b7e629030ba6899607c7ba878a52f89dc9652a8
Raviteja Mannam <ravitejamannam@Ravitejas-MacBook-Air.local>
2024-12-28 00:48:00 +0530

    jwt

--- backend/middleware/user.js
+++ backend/middleware/user.js
@@ -1,21 +1,19 @@
-const jwt = require('jsonwebtoken');
-const {JWT_USER_PASSWORD} = require('../config');
-function userMiddleware(req, res, next) {
-    const token = req.headers.token;
-    const decoded = jwt.verify(token, JWT_USER_PASSWORD);
+const jwt = require("jsonwebtoken");
+const { JWT_USER_PASSWORD } = require("../config");
 
+const userMiddleware = (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
 
+    try {
+        const decoded = jwt.verify(token, JWT_USER_PASSWORD);
+        req.userId = decoded.id;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "Invalid token" });
+    }
+};
 
-if(decoded) {
-    req.userId = decoded.id;
-    next()
-} else {
-    res.status(403).json({
-        message: "Invalid token"
-    })
-}
-
-}
-module.exports = {
-    userMiddleware: userMiddleware,
-}
+module.exports = { userMiddleware };
