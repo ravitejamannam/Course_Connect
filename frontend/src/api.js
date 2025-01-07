5789d68ee235f2637ec01a68f2c0b358b04c64b6
Raviteja Mannam <ravitejamannam88@gmail.com>
2025-01-07 21:56:28 +0530

    modified frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,10 +2,6 @@ import axios from 'axios';
 
 const API = axios.create({
     baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/api/v1',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    withCredentials: true
 });
 
 API.interceptors.request.use((req) => {
